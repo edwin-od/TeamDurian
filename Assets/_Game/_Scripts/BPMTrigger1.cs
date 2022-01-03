8d1e841c6b76a3f5b91ac02b4abda82bee0ec98d
edwin-od <edwin.odeimi@lau.edu>
2022-01-04 00:57:43 +0100

    Added Grid Movement

--- Assets/_Game/_Scripts/BPMTrigger1.cs
+++ Assets/_Game/_Scripts/BPMTrigger1.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BPMTrigger1 : TempoTrigger
+public class BPMTrigger1 : GridMoveable
 {
     public override void Beat()
     {
-        Debug.Log("Beat 1 " + Tempo.ElapsedTime());
+        Debug.Log("Beat 1 " + Tempo.ElapsedTime);
     }
+
+    private void Update()
+    {
+        if (!IsMoving)
+        {
+            if (Input.GetKey(KeyCode.W))
+                StartCoroutine(Move(DIRECTION.UP));
+            if (Input.GetKey(KeyCode.S))
+                StartCoroutine(Move(DIRECTION.DOWN));
+            if (Input.GetKey(KeyCode.D))
+                StartCoroutine(Move(DIRECTION.RIGHT));
+            if (Input.GetKey(KeyCode.A))
+                StartCoroutine(Move(DIRECTION.LEFT));
+        }
+    }
+
 }
